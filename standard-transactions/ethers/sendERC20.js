3b4451ba81672b9e6c59b857ecbf4f7acd276111
Ronan McGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2022-08-26 13:56:03 +0100

    erc20 transfer now working

--- standard-transactions/ethers/sendERC20.js
+++ standard-transactions/ethers/sendERC20.js
@@ -1,4 +1,5 @@
 const ethers = require("ethers");
+require("dotenv").config();
 
 //Code taken from "https://www.web3byexample.com/transferring-erc20"
 
@@ -16,27 +17,26 @@ const ethers = require("ethers");
     "event Transfer(address indexed from, address indexed to, uint amount)",
   ];
 
-  console.log(abi); //added by Ronan
-
   const usdc = "0x07865c6E87B9F70255377e024ace6630C1Eaa37F"; //updated by Ronan to Goerli USDC address
-  const usdcDecimals = 6;
+  const usdcDecimals = 6; //USDC only has 6, not 18 decimals.
   const network = process.env.ETHEREUM_NETWORK;
-  const provider =  new ethers.providers.InfuraProvider(
+  const provider =  new ethers.providers.InfuraProvider( //Creates a new provider/connection
     network,
     process.env.INFURA_PROJECT_ID
   );
   const privateKey = process.env.SIGNER_PRIVATE_KEY;
-  const wallet = new ethers.Wallet(privateKey, provider);
-  const erc20_rw = new ethers.Contract(usdc, abi, wallet);
+  const wallet = new ethers.Wallet(privateKey, provider); //Creates a wallet (which is a signer via inheritance) using the provider
+  const erc20_rw = new ethers.Contract(usdc, abi, wallet); //Creates a contract instance that includes a signer (via the wallet)
 
   const receipt = "0x1E2eBeBB3348B1FeFC29239c20Df1c78668180Cc"; //Ronan's Goerli Test Wallet Hot June 2022
-  const amount = ethers.utils.parseUnits("0.1", usdcDecimals);
-  const tx = await erc20_rw.transfer(receipt, amount);
+  const amount = ethers.utils.parseUnits("0.1", usdcDecimals); //multiplies by 1e6, the number of decimals.
+  const tx = await erc20_rw.transfer(receipt, amount); //calling contract.transfer where transfer is a function in the USDC contract.
+  //not clear how the contract knows that this has been signed...maybe because there is an isOwner?
   console.log("tx has been sent, waiting for confirmation. hash is ", tx.hash);
 
   await tx.wait();
 
-    console.log(
+  console.log(
     "balance",
     ethers.utils.formatUnits(await erc20_rw.balanceOf(receipt), usdcDecimals)
   );
