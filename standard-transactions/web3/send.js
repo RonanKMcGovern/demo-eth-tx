ec5b5403084aecfaf9bb116ed6025a20ed63faea
Ronan McGovern <78278410+RonanKMcGovern@users.noreply.github.com>
2022-08-26 15:34:14 +0100

    web3 transfer using ganache network

--- standard-transactions/web3/send.js
+++ standard-transactions/web3/send.js
@@ -1,13 +1,23 @@
 const Web3 = require("web3");
+network = 'ganache'
 
 async function main() {
   // Configuring the connection to an Ethereum node
-  const network = process.env.ETHEREUM_NETWORK;
-  const web3 = new Web3(
-    new Web3.providers.HttpProvider(
-      `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
-    )
+
+// Is there is an injected web3 instance?
+
+
+if (typeof web3 !== 'undefined') {
+  const web3 = new Web3(web3.currentProvider);
+  console.log("web3 is defined and there is a provider")
+} else {
+  // If no injected web3 instance is detected, fallback to Ganache CLI.
+  web3 = new Web3(
+    new Web3.providers.HttpProvider('http://127.0.0.1:7545')
   );
+  console.log("web3 is not defined. Setting Ganache as provider")
+}
+  
   // Creating a signing account from a private key
   const signer = web3.eth.accounts.privateKeyToAccount(
     process.env.SIGNER_PRIVATE_KEY
@@ -16,18 +26,18 @@ async function main() {
   // Creating the transaction object
   const tx = {
     from: signer.address,
-    to: "0x1E2eBeBB3348B1FeFC29239c20Df1c78668180Cc", //Ronan's Goerli Test Wallet Hot June 2022
-    value: web3.utils.toWei("0.001"),
+    to: "0x8458F09074482d6baCBbeD88E84Acb676205280A", //Ganache wallet #2
+    value: web3.utils.toWei("1"),
   };
   // Assigning the right amount of gas
   tx.gas = await web3.eth.estimateGas(tx);
 
   // Sending the transaction to the network
   const receipt = await web3.eth
-    .sendTransaction(tx)
+    .sendTransaction(tx) //Somehow this transaction is being signed by the private key.
     .once("transactionHash", (txhash) => {
       console.log(`Mining transaction ...`);
-      console.log(`https://${network}.etherscan.io/tx/${txhash}`);
+      console.log(`tx ID: ${txhash}`);
     });
   // The transaction is now on chain!
   console.log(`Mined in block ${receipt.blockNumber}`);
